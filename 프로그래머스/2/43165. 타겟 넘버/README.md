7c582ac50131f87b9692e5274d30e924de4b6086
ChoiMJ <97894470+Acho-mj@users.noreply.github.com>
2025-10-29 22:29:00 +0900

    [level 2] Title: 타겟 넘버, Time: 5.68 ms, Memory: 97.6 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 86.2 MB, 시간: 9.38 ms
+메모리: 97.6 MB, 시간: 5.68 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 24일 17:58:45
+2025년 10월 29일 22:28:57
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -2,21 +2,17 @@ class Solution {
     int[] numbers;
     int target;
     int answer;
-    
-    public void dfs(int index, int sum){
-        
-        // 재귀 탈출 조건
-        if(index == numbers.length){
+    private void dfs(int index, int sum){        
+        // 탈출조건
+        if(index==numbers.length){
             if(sum==target) answer++;
             return;
         }
-            
-        // 수행 동작(덧셈, 뺄셈)
-        dfs(index + 1, sum + numbers[index]);
-        dfs(index + 1, sum - numbers[index]);
-           
+        
+        // 수행동작
+        dfs(index+1, sum+numbers[index]);
+        dfs(index+1, sum-numbers[index]);
     }
-    
     public int solution(int[] numbers, int target) {
         answer = 0;
         this.numbers = numbers;
