47e60d0bbc064594616106afa3d8a7b4ad30913f
ChoiMJ <97894470+Acho-mj@users.noreply.github.com>
2026-02-13 15:51:39 +0900

    [Silver I] Title: 경로 찾기, Time: 156 ms, Memory: 15008 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n;
+    static int[][] dist;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+
+        dist = new int[n+1][n+1];
+        for(int i=1; i<=n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j=1; j<=n; j++) {
+                dist[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        floyd();
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<=n; i++) {
+            for (int j=1; j<=n; j++) {
+                sb.append(dist[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    static void floyd(){
+        for(int k=1; k<=n; k++){
+            for(int i=1; i<=n; i++){
+                for(int j=1; j<=n; j++){
+                    if(dist[i][k] == 1 && dist[k][j] == 1){
+                        dist[i][j] = 1;
+                    }
+                }
+            }
+        }
+    }
+}
